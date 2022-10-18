fac7de91b6e1cd943ac5e8d6c618eae3892b1b4f
ThomasRossouw <106060643+ThomasRossouw@users.noreply.github.com>
2022-10-18 21:45:16 +0200

    Add project files.

---
+++ PROG7312_ST10121910/Mix.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PROG7312_ST10121910
+{
+    /// <summary>
+    /// Interaction logic for Mix.xaml
+    /// </summary>
+    public partial class Mix : Window
+    {
+        public Mix()
+        {
+            InitializeComponent();
+        }
+    }
+}
